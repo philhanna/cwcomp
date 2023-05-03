59f140f9a7a1f24449d4f32c223f0dc20c6113e6
Phil Hanna <ph1204@gmail.com>
2023-05-03 13:42:49 -0400

    Added unit tests for Compare

--- model/point_test.go
+++ model/point_test.go
@@ -11,12 +11,20 @@ func TestPoint_Compare(t *testing.T) {
 		want int
 	}{
 		{"same point", Point{Row: 1, Col: 3}, Point{Row: 1, Col: 3}, 0},
+		{"this column is greater", Point{Row: 1, Col: 3}, Point{Row: 1, Col: 2}, 1},
+		{"this column is less", Point{Row: 1, Col: 3}, Point{Row: 1, Col: 4}, -1},
+		{"this row is greater", Point{Row: 1, Col: 3}, Point{Row: 0, Col: 4}, 1},
+		{"this row is less", Point{Row: 0, Col: 3}, Point{Row: 1, Col: 0}, -1},
+		{"same row, col less", Point{Row: 0, Col: 0}, Point{Row: 0, Col: 4}, -1},
+		{"same row, col more", Point{Row: 0, Col: 0}, Point{Row: 0, Col: -4}, 1},
+		{"same col, row less", Point{2, 3}, Point{3, 3}, -1},
+		{"same col, row more", Point{4, 3}, Point{3, 3}, 1},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			this := tt.this
 			that := tt.that
-			assert.Equal(t, 0, this.Compare(that))
+			assert.Equal(t, tt.want, this.Compare(that))
 		})
 	}
 }
