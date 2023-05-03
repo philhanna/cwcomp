809c6a64d7be380b9685e5a1ec2350b6534c9fd8
Phil Hanna <ph1204@gmail.com>
2023-05-03 13:51:28 -0400

    Added unit tests for Equal

--- model/point_test.go
+++ model/point_test.go
@@ -28,3 +28,29 @@ func TestPoint_Compare(t *testing.T) {
 		})
 	}
 }
+
+func TestPoint_Equal(t *testing.T) {
+	tests := []struct {
+		name string
+		this Point
+		that Point
+		want bool
+	}{
+		{"same point", Point{Row: 1, Col: 3}, Point{Row: 1, Col: 3}, true},
+		{"this column is greater", Point{Row: 1, Col: 3}, Point{Row: 1, Col: 2}, false},
+		{"this column is less", Point{Row: 1, Col: 3}, Point{Row: 1, Col: 4}, false},
+		{"this row is greater", Point{Row: 1, Col: 3}, Point{Row: 0, Col: 4}, false},
+		{"this row is less", Point{Row: 0, Col: 3}, Point{Row: 1, Col: 0}, false},
+		{"same row, col less", Point{Row: 0, Col: 0}, Point{Row: 0, Col: 4}, false},
+		{"same row, col more", Point{Row: 0, Col: 0}, Point{Row: 0, Col: -4}, false},
+		{"same col, row less", Point{2, 3}, Point{3, 3}, false},
+		{"same col, row more", Point{4, 3}, Point{3, 3}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			this := tt.this
+			that := tt.that
+			assert.Equal(t, tt.want, this.Equal(that))
+		})
+	}
+}
