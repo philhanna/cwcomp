7f6570e46bd1932d3fec1bc3ba8d91b55a17dceb
Phil Hanna <ph1204@gmail.com>
2023-05-04 11:18:37 -0400

    Added unit tests for CalculateWordNumbers

---
+++ grid/grid_test.go
@@ -0,0 +1,30 @@
+package grid
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func getGoodGrid() *Grid {
+	p := NewGrid(9)
+	p.bclist = []Point{
+		{1, 1}, {1, 5},
+		{2, 5},
+		{3, 5},
+		{4, 9},
+		{5, 1}, {5, 2}, {5, 3},
+		{5, 7}, {5, 8}, {5, 9},
+		{6, 1},
+		{7, 5},
+		{8, 5},
+		{9, 5}, {9, 9},
+	}
+	p.CalculateWordNumbers()
+	return p
+}
+
+func TestGrid_CalculateWordNumbers(t *testing.T) {
+	grid := getGoodGrid()
+	assert.Equal(t, 25, len(grid.nclist))
+}
