f2a859f3789ccc1b29b9fc73e76f9decbf1f168c
Phil Hanna <ph1204@gmail.com>
2023-05-06 19:56:54 -0400

    Added unit test for String method

--- grid/letter_cell.go
+++ grid/letter_cell.go
@@ -25,11 +25,18 @@ type LetterCell struct {
 
 // LetterCell returns a string representation of this letter cell.
 func (lc *LetterCell) String() string {
-	parts := []string{
-		fmt.Sprintf(`ncAcross:%p`, lc.ncAcross),
-		fmt.Sprintf(`ncDown:%p`, lc.ncDown),
-		fmt.Sprintf(`letter:%q`, lc.letter),
+	parts := make([]string, 0)
+	if lc.ncAcross == nil {
+		parts = append(parts, fmt.Sprintf(`ncAcross:%v`, nil))
+	} else {
+		parts = append(parts, fmt.Sprintf(`ncAcross:%v`, lc.ncAcross.String()))
 	}
+	if lc.ncDown == nil {
+		parts = append(parts, fmt.Sprintf(`ncDown:%v`, nil))
+	} else {
+		parts = append(parts, fmt.Sprintf(`ncDown:%v`, lc.ncDown.String()))
+	}
+	parts = append(parts, fmt.Sprintf("letter:%q", lc.letter))
 	s := strings.Join(parts, ", ")
 	return s
 }
