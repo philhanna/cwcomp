8ce436b0bcfef94fd03808fd007a40e09a39a1eb
Phil Hanna <ph1204@gmail.com>
2023-05-07 00:11:08 -0400

    gofmt -w -s

--- grid/numbered_cell_test.go
+++ grid/numbered_cell_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNumberedCell_String(t *testing.T) {	
+func TestNumberedCell_String(t *testing.T) {
 	type fields struct {
 		LetterCell LetterCell
 		seq        int
@@ -23,12 +23,12 @@ func TestNumberedCell_String(t *testing.T) {
 			fields{
 				LetterCell{
 					ncAcross: nil,
-					ncDown: nil,
-					letter: "A",
+					ncDown:   nil,
+					letter:   "A",
+				},
+				1, 3, 4,
 			},
-			1, 3, 4,
-		},
-		"letterCell:{<nil> <nil> A}, seq:1, aLen:3, dLen:4"},
+			"letterCell:{<nil> <nil> A}, seq:1, aLen:3, dLen:4"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
