63381fa2f831dfa949df7604e1669a56272d9c54
Phil Hanna <ph1204@gmail.com>
2023-05-10 21:34:35 -0400

    Moved DumpGrid() to test

--- model/grid.go
+++ model/grid.go
@@ -2,33 +2,9 @@ package model
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/philhanna/stack"
 )
-
-func DumpGrid(grid *Grid) {
-	n := grid.n
-	for r := 1; r <= n; r++ {
-		for c := 1; c <= n; c++ {
-			point := Point{r, c}
-			cell := grid.GetCell(point)
-			switch cellType := cell.(type) {
-			case BlackCell:
-				bc := cell.(BlackCell)
-				log.Printf("BlackCell    at %v has value %v\n", point, bc.String())
-			case LetterCell:
-				lc := cell.(LetterCell)
-				log.Printf("LetterCell   at %v has value %v\n", point, lc.String())
-			case NumberedCell:
-				nc := cell.(NumberedCell)
-				log.Printf("NumberedCell at %v has value %v\n", point, nc.String())
-			default:
-				log.Printf("???????????  at %v is type %s, value %v\n", point, cellType, cell)
-			}
-		}
-	}
-}
 
 // ---------------------------------------------------------------------
 // Type definitions
