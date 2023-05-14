e70ad2ba77757173646c8d41e16bb80580d419a5
Phil Hanna <ph1204@gmail.com>
2023-05-14 15:35:12 -0400

    Added tests for SetLetter

--- model/grid_test.go
+++ model/grid_test.go
@@ -65,73 +65,7 @@ func TestGrid_CountBlackCells(t *testing.T) {
 	// 18 because we added a point and its symmetric point
 }
 
-func TestGrid_GetWordLength(t *testing.T) {
-	grid := getGoodGrid()
-	tests := []struct{
-		name string
-		seq int
-		dir Direction
-		want int
-	}{
-		{"Good both(a)", 4, ACROSS, 4},
-		{"Good both(d)", 4, DOWN, 9},
-		{"Good across", 25, ACROSS, 3},
-		{"Good down", 19, DOWN, 3},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T){
-			assert.Equal(t, tt.want, grid.GetWordLength(tt.seq, tt.dir))
-		})
-	}
-}
-
-func TestGrid_GetWordLength_Bad(t *testing.T) {
-	grid := getGoodGrid()
-	assert.Panics(t, func() {
-		grid.GetWordLength(-3, ACROSS)
-	})
-}
-
-func TestGrid_GetAcrossWordText(t *testing.T) {
-	grid := getGoodGrid()
-
-	// It should fail if the word sequence number is invalid
-	assert.Panics(t, func() {
-		grid.GetWordText(-1, ACROSS)
-	})
-	assert.Panics(t, func() {
-		grid.GetWordText(1000, ACROSS)
-	})
-
-	// Should return a string of the correct length
-
-	wantLength := 3
-	want := strings.Repeat(" ", wantLength)
-	have := grid.GetWordText(14, ACROSS)
-	assert.Equal(t, wantLength, len(have))
-	assert.Equal(t, want, have)
-}
-
-func TestGrid_GetDownWordText(t *testing.T) {
-	grid := getGoodGrid()
-
-	// It should fail if the word sequence number is invalid
-	assert.Panics(t, func() {
-		grid.GetWordText(-1, DOWN)
-	})
-	assert.Panics(t, func() {
-		grid.GetWordText(1000, DOWN)
-	})
-
-	// Should return a string of the correct length
-	wantLength := 9
-	want := strings.Repeat(" ", wantLength)
-	have := grid.GetWordText(3, DOWN)
-	assert.Equal(t, wantLength, len(have))
-	assert.Equal(t, want, have)
-}
-
-func TestGrid_GetWordClue(t *testing.T) {
+func TestGrid_GetClue(t *testing.T) {
 	tests := []struct {
 		name string
 		seq  int
@@ -146,27 +80,55 @@ func TestGrid_GetWordClue(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			grid := getGoodGrid()
-			have := grid.GetWordClue(tt.seq, tt.dir)
+			have := grid.GetClue(tt.seq, tt.dir)
 			assert.Equal(t, tt.want, have)
 		})
 	}
 }
 
-func TestGrid_GetWordClue_Bad(t *testing.T) {
+func TestGrid_GetClue_Bad(t *testing.T) {
 	grid := getGoodGrid()
 	assert.Panics(t, func() {
-		grid.GetWordClue(-1, ACROSS)
+		grid.GetClue(-1, ACROSS)
 	})
 }
 
-func TestGrid_GetWordText(t *testing.T) {
+func TestGrid_GetLength(t *testing.T) {
+	grid := getGoodGrid()
 	tests := []struct {
+		name string
+		seq  int
+		dir  Direction
+		want int
+	}{
+		{"Good both(a)", 4, ACROSS, 4},
+		{"Good both(d)", 4, DOWN, 9},
+		{"Good across", 25, ACROSS, 3},
+		{"Good down", 19, DOWN, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, grid.GetLength(tt.seq, tt.dir))
+		})
+	}
+}
+
+func TestGrid_GetLength_Bad(t *testing.T) {
+	grid := getGoodGrid()
+	assert.Panics(t, func() {
+		grid.GetLength(-3, ACROSS)
+	})
+}
+
+func TestGrid_GetText(t *testing.T) {
+	type test struct {
 		name       string
 		seq        int
 		dir        Direction
 		wantLength int
 		want       string
-	}{
+	}
+	tests := []test{
 		{"Good across", 21, ACROSS, 4, "    "},
 		{"Good down", 19, DOWN, 3, "   "},
 		{"No across word", 13, ACROSS, 0, ""},
@@ -176,10 +138,80 @@ func TestGrid_GetWordText(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			grid := getGoodGrid()
 			want := tt.want
-			have := grid.GetWordText(tt.seq, tt.dir)
+			have := grid.GetText(tt.seq, tt.dir)
 			assert.Equal(t, tt.wantLength, len(have))
 			assert.Equal(t, want, have)
 		})
+	}
+}
+
+func TestGrid_GetText_Across(t *testing.T) {
+	grid := getGoodGrid()
+
+	// It should fail if the word sequence number is invalid
+	assert.Panics(t, func() {
+		grid.GetText(-1, ACROSS)
+	})
+	assert.Panics(t, func() {
+		grid.GetText(1000, ACROSS)
+	})
+
+	// Should return a string of the correct length
+
+	wantLength := 3
+	want := strings.Repeat(" ", wantLength)
+	have := grid.GetText(14, ACROSS)
+	assert.Equal(t, wantLength, len(have))
+	assert.Equal(t, want, have)
+}
+
+func TestGrid_GetText_Down(t *testing.T) {
+	grid := getGoodGrid()
+
+	// It should fail if the word sequence number is invalid
+	assert.Panics(t, func() {
+		grid.GetText(-1, DOWN)
+	})
+	assert.Panics(t, func() {
+		grid.GetText(1000, DOWN)
+	})
+
+	// Should return a string of the correct length
+	wantLength := 9
+	want := strings.Repeat(" ", wantLength)
+	have := grid.GetText(3, DOWN)
+	assert.Equal(t, wantLength, len(have))
+	assert.Equal(t, want, have)
+}
+
+func TestGrid_GetTextWithLetters(t *testing.T) {
+	type test struct {
+		name       string
+		seq        int
+		dir        Direction
+		wantLength int
+		want       string
+	}
+	grid := getGoodGrid()
+	grid.SetLetter(Point{5, 4}, "O")
+	grid.SetLetter(Point{5, 5}, "A")
+	grid.SetLetter(Point{5, 6}, "F")
+
+	tests := []test{
+		{"14 across", 14, ACROSS, 3, "OAF"},
+		{"3 down", 3, DOWN, 9, "    O    "},
+		{"13 down", 13, DOWN, 3, " A "},
+		{"4 down", 4, DOWN, 9, "    F    "},
+		{"No change to others", 15, ACROSS, 8, "        "},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			want := tt.want
+			have := grid.GetText(tt.seq, tt.dir)
+			assert.Equal(t, tt.wantLength, len(have))
+			assert.Equal(t, want, have)
+		})
+
 	}
 }
 
