047ddc1d04b441569ae4b82c376e291fa447b8d3
Phil Hanna <ph1204@gmail.com>
2023-05-14 14:51:08 -0400

    Fix for issue #9
    Added unit test

--- model/grid_test.go
+++ model/grid_test.go
@@ -118,34 +118,34 @@ func TestGrid_GetDownWordText(t *testing.T) {
 
 func TestGrid_GetWordClue(t *testing.T) {
 	tests := []struct {
-		name       string
-		seq        int
-		dir        Direction
-		want       string
-		}{
-			{"Good across", 21, ACROSS, ""},
-			{"Good down", 19, DOWN, ""},
-			{"No across word", 13, ACROSS, ""},
-			{"No down word", 21, DOWN, ""},
-		}
-		for _, tt := range tests {
-			t.Run(tt.name, func(t *testing.T) {
-				grid := getGoodGrid()
-				have := grid.GetWordClue(tt.seq, tt.dir)
-				assert.Equal(t, tt.want, have)
-			})
-		}
+		name string
+		seq  int
+		dir  Direction
+		want string
+	}{
+		{"Good across", 21, ACROSS, ""},
+		{"Good down", 19, DOWN, ""},
+		{"No across word", 13, ACROSS, ""},
+		{"No down word", 21, DOWN, ""},
 	}
-	
-	func TestGrid_GetWordClue_Bad(t *testing.T) {
-		grid := getGoodGrid()
-		assert.Panics(t, func() {
-			grid.GetWordClue(-1, ACROSS)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			grid := getGoodGrid()
+			have := grid.GetWordClue(tt.seq, tt.dir)
+			assert.Equal(t, tt.want, have)
 		})
 	}
-	
-	func TestGrid_GetWordText(t *testing.T) {
-		tests := []struct {
+}
+
+func TestGrid_GetWordClue_Bad(t *testing.T) {
+	grid := getGoodGrid()
+	assert.Panics(t, func() {
+		grid.GetWordClue(-1, ACROSS)
+	})
+}
+
+func TestGrid_GetWordText(t *testing.T) {
+	tests := []struct {
 		name       string
 		seq        int
 		dir        Direction
