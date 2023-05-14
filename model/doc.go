4162707625c9205ca41835664d4c87a82a84be75
Phil Hanna <ph1204@gmail.com>
2023-05-14 16:02:22 -0400

    Updated doc.go with up to date info

--- model/doc.go
+++ model/doc.go
@@ -1,14 +1,13 @@
 // Types and functions that support the internal workings of the
 // application.
 //
-// A grid consists of an nxn matrix of cells, which are of three types:
+// A grid consists of an nxn matrix of cells, which are of two types:
 //   - Black cells: Blocks in the grid
+//
 //   - Letter cells: Ordinary cells where letters of words can be placed.
-//     They also contain a pair of pointers: one to the numbered cell that
+//     They also contain a pair of pointers: one to the word number that
 //     begins the across word that contains this cell, and another to the
-//     numbered cell that begins the down word that contains this cell.
-//   - Numbered cells: Letter cells that mark the beginning of an across
-//     and/or down word.
+//     word number that begins the down word that contains this cell.
 //
 // The grid also supports undo/redo for black cells in this grid.
 package model
