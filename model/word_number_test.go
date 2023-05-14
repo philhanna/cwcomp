e0e864c72206d1313b39f52f03a0474d2a8644dd
Phil Hanna <ph1204@gmail.com>
2023-05-14 13:32:09 -0400

    Simplified String() unit test

--- model/word_number_test.go
+++ model/word_number_test.go
@@ -1,32 +1,14 @@
 package model
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestWordNumber_String(t *testing.T) {
-	type fields struct {
-		seq   int
-		point Point
-		aLen  int
-		dLen  int
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   string
-	}{
-		{"1 across", fields{1, Point{1, 2}, 3, 4}, `seq:1,point:{r:1,c:2},aLen:3,dLen:4`},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			wn := &WordNumber{
-				seq:   tt.fields.seq,
-				point: tt.fields.point,
-				aLen:  tt.fields.aLen,
-				dLen:  tt.fields.dLen,
-			}
-			if got := wn.String(); got != tt.want {
-				t.Errorf("WordNumber.String() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+	wn := NewWordNumber(1, Point{1, 2}, 3, 4)
+	want := `seq:1,point:{r:1,c:2},aLen:3,dLen:4,aClue:"",dClue:""`
+	have := wn.String()
+	assert.Equal(t, want, have)
 }
