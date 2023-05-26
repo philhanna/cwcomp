94a71de286b4495ab4805b646dda9935af3e9cb6
Phil Hanna <ph1204@gmail.com>
2023-05-25 23:35:42 -0400

    Started create.go

---
+++ svg/create.go
@@ -0,0 +1,16 @@
+package svg
+
+// ---------------------------------------------------------------------
+// Type definitions
+// ---------------------------------------------------------------------
+
+type SVG struct { 
+	/*
+	n int // Number of either rows or columns in the grid (assumed to be the same)
+	cells [][]byte // Simple matrix representation of the grid
+	*/
+}
+
+// ---------------------------------------------------------------------
+// Constructor
+// ---------------------------------------------------------------------
