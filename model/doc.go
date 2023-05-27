ccf1f56ca7acbecd1419cd63ffc8d7b2f9a924c4
Phil Hanna <ph1204@gmail.com>
2023-05-27 14:23:33 -0400

    Updated comment

--- model/doc.go
+++ model/doc.go
@@ -6,10 +6,7 @@
 //   - Black cells: Blocks in the grid
 //
 //   - Letter cells: Ordinary cells where letters of words can be
-//     placed.  They also contain a pair of pointers: one to the word
-//     number that begins the across word that contains this cell, and
-//     another to the word number that begins the down word that contains
-//     this cell.
+//     placed.
 //
 // The grid also supports undo/redo for black cells in this grid.
 package model
