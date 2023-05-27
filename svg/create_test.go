fd569695ea4d27cae4faa73bb8f8a0a63afc741b
Phil Hanna <ph1204@gmail.com>
2023-05-27 18:49:28 -0400

    Added black cells and letters for 100% test coverage

--- svg/create_test.go
+++ svg/create_test.go
@@ -7,18 +7,19 @@ import (
 	"github.com/philhanna/cwcomp/model"
 )
 
+const BLK = byte('\x00')
+
 func getGoodGrid() [][]byte {
-	blk := byte('\x00')
 	cells := [][]byte{
-		{blk, 'N', 'O', 'W', blk, ' ', ' ', ' ', 'C'},
-		{'B', 'L', 'U', 'E', blk, ' ', ' ', ' ', 'O'},
-		{' ', ' ', ' ', ' ', blk, ' ', ' ', ' ', 'W'},
-		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', blk},
-		{blk, blk, blk, ' ', ' ', ' ', blk, blk, blk},
-		{blk, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		{'H', ' ', ' ', ' ', blk, ' ', ' ', ' ', ' '},
-		{'O', ' ', ' ', ' ', blk, ' ', ' ', ' ', ' '},
-		{'W', ' ', ' ', ' ', blk, ' ', ' ', ' ', blk},
+		{BLK, 'N', 'O', 'W', BLK, ' ', ' ', ' ', 'C'},
+		{'B', 'L', 'U', 'E', BLK, ' ', ' ', ' ', 'O'},
+		{' ', ' ', ' ', ' ', BLK, ' ', ' ', ' ', 'W'},
+		{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', BLK},
+		{BLK, BLK, BLK, ' ', ' ', ' ', BLK, BLK, BLK},
+		{BLK, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+		{'H', ' ', ' ', ' ', BLK, ' ', ' ', ' ', ' '},
+		{'O', ' ', ' ', ' ', BLK, ' ', ' ', ' ', ' '},
+		{'W', ' ', ' ', ' ', BLK, ' ', ' ', ' ', BLK},
 	}
 	return cells
 }
@@ -28,15 +29,26 @@ func TestSVG_GenerateSVG(t *testing.T) {
 	svg := NewSVG(cells)
 	have := svg.GenerateSVG()
 	if true { // Change this to true if you want to write the file
-		os.WriteFile("test.svg", []byte(have), 0644)
+		os.WriteFile("testdata/simple_matrix.svg", []byte(have), 0644)
 	}
 }
 
 func TestSVG_NewSVGFromGrid(t *testing.T) {
 	grid := model.NewGrid(9)
+	blackCells := [][]int {
+		{1, 1}, {1, 5}, {2, 5}, {3, 5}, {4, 9}, {5, 1}, {5, 2}, {5, 3},
+	}
+	for _, pair := range blackCells {
+		r, c := pair[0], pair[1]
+		point := model.NewPoint(r, c)
+		grid.Toggle(point)
+	}
+	grid.SetLetter(model.NewPoint(5, 4), "O")
+	grid.SetLetter(model.NewPoint(5, 5), "A")
+	grid.SetLetter(model.NewPoint(5, 6), "F")
 	svg := NewSVGFromGrid(grid)
 	have := svg.GenerateSVG()
-	if false { // Change this to true if you want to write the file
-		os.WriteFile("test.svg", []byte(have), 0644)
-	}		
+	if true { // Change this to true if you want to write the file
+		os.WriteFile("testdata/from_grid.svg", []byte(have), 0644)
+	}
 }
