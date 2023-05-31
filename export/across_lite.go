ae19b179e85917eaf75bd1151fbffc5d1bb3a745
Phil Hanna <ph1204@gmail.com>
2023-05-30 20:26:48 -0400

    Working on FSM

--- export/across_lite.go
+++ export/across_lite.go
@@ -1,5 +1,12 @@
 package export
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 // ---------------------------------------------------------------------
 // Type definitions
 // ---------------------------------------------------------------------
@@ -46,6 +53,84 @@ func NewAcrossLite() *AcrossLite {
 
 // Import parses the text in an AcrossLite puzzle
 func Import(filename string) (*AcrossLite, error) {
-	p := NewAcrossLite()
-	return p, nil
+	pal := NewAcrossLite()
+
+	fp, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	type ParsingState byte
+	const (
+		INIT ParsingState = iota
+		LOOKING_FOR_TITLE
+		READING_TITLE
+		LOOKING_FOR_AUTHOR
+		READING_AUTHOR
+		LOOKING_FOR_COPYRIGHT
+		READING_COPYRIGHT
+		LOOKING_FOR_SIZE
+		READING_SIZE
+	)
+	state := INIT
+
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan() {
+
+		line := scanner.Text()
+		line = strings.TrimSpace(line)
+
+		switch state {
+
+		case INIT:
+			if line == "<ACROSS PUZZLE>" {
+				state = LOOKING_FOR_TITLE
+			} else {
+				return nil, fmt.Errorf("Not a valid AcrossLite text file: %s", line)
+			}
+
+		case LOOKING_FOR_TITLE:
+			if line == "<TITLE>" {
+				state = READING_TITLE
+			} else {
+				return nil, fmt.Errorf("did not find <TITLE>")
+			}
+
+		case READING_TITLE:
+			pal.title = line
+			state = LOOKING_FOR_AUTHOR
+
+		case LOOKING_FOR_AUTHOR:
+			if line == "<AUTHOR>" {
+				state = READING_AUTHOR
+			} else {
+				return nil, fmt.Errorf("did not find <AUTHOR>")
+			}
+
+		case READING_AUTHOR:
+			pal.author = line
+			state = LOOKING_FOR_COPYRIGHT
+
+		case LOOKING_FOR_COPYRIGHT:
+			if line == "<COPYRIGHT>" {
+				state = READING_COPYRIGHT
+			} else {
+				return nil, fmt.Errorf("did not find <COPYRIGHT>")
+			}
+
+		case READING_COPYRIGHT:
+			pal.copyright = line
+			state = LOOKING_FOR_SIZE
+
+		case LOOKING_FOR_SIZE:
+			if line == "<SIZE>" {
+				state = READING_SIZE
+			} else {
+				return nil, fmt.Errorf("did not find <SIZE>")
+			}
+
+		}
+	}
+
+	return pal, nil
 }
