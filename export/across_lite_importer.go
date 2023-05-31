08835088327db40305b6038ec84398c307438b2a
Phil Hanna <ph1204@gmail.com>
2023-05-31 13:50:35 -0400

    Split across lite into struct file and importer file

---
+++ export/across_lite_importer.go
@@ -0,0 +1,122 @@
+package export
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// ---------------------------------------------------------------------
+// Functions
+// ---------------------------------------------------------------------
+
+// Import parses the text in an AcrossLite puzzle
+func Import(filename string) (*AcrossLite, error) {
+	pal := NewAcrossLite()
+
+	fp, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	type ParsingState byte
+	const (
+		INIT ParsingState = iota
+		LOOKING_FOR_TITLE
+		READING_TITLE
+		LOOKING_FOR_AUTHOR
+		READING_AUTHOR
+		LOOKING_FOR_COPYRIGHT
+		READING_COPYRIGHT
+		LOOKING_FOR_SIZE
+		READING_SIZE
+		LOOKING_FOR_GRID
+		READING_GRID
+	)
+	state := INIT
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan() {
+
+		line := scanner.Text()
+		line = strings.TrimSpace(line)
+
+		switch state {
+
+		case INIT:
+			if line == "<ACROSS PUZZLE>" {
+				state = LOOKING_FOR_TITLE
+			} else {
+				return nil, fmt.Errorf("Not a valid AcrossLite text file: %s", line)
+			}
+
+		case LOOKING_FOR_TITLE:
+			if line == "<TITLE>" {
+				state = READING_TITLE
+			} else {
+				return nil, fmt.Errorf("did not find <TITLE>")
+			}
+
+		case READING_TITLE:
+			pal.title = line
+			state = LOOKING_FOR_AUTHOR
+
+		case LOOKING_FOR_AUTHOR:
+			if line == "<AUTHOR>" {
+				state = READING_AUTHOR
+			} else {
+				return nil, fmt.Errorf("did not find <AUTHOR>")
+			}
+
+		case READING_AUTHOR:
+			pal.author = line
+			state = LOOKING_FOR_COPYRIGHT
+
+		case LOOKING_FOR_COPYRIGHT:
+			if line == "<COPYRIGHT>" {
+				state = READING_COPYRIGHT
+			} else {
+				return nil, fmt.Errorf("did not find <COPYRIGHT>")
+			}
+
+		case READING_COPYRIGHT:
+			pal.copyright = line
+			state = LOOKING_FOR_SIZE
+
+		case LOOKING_FOR_SIZE:
+			if line == "<SIZE>" {
+				state = READING_SIZE
+			} else {
+				return nil, fmt.Errorf("did not find <SIZE>")
+			}
+
+		case READING_SIZE:
+			reSize := regexp.MustCompile(`(\d+)x(\d)`)
+			tokens := reSize.FindStringSubmatch(line)
+			if tokens == nil {
+				return nil, fmt.Errorf("no <digits>x<digits> size expression found")
+			} else if len(tokens) != 2 {
+				return nil, fmt.Errorf("expected two size integers, found %d", len(tokens))
+			} else if tokens[0] != tokens[1] {
+				return nil, fmt.Errorf("only square grids allowed, not %sx%s", tokens[0], tokens[1])
+			} else {
+				pal.size, _ = strconv.Atoi(tokens[0])
+				state = LOOKING_FOR_GRID
+			}
+
+		case LOOKING_FOR_GRID:
+			if line == "<GRID>" {
+				state =  READING_GRID	
+			} else {
+				return nil, fmt.Errorf("did not find <GRID>")
+			}
+
+		case READING_GRID:
+			
+		}
+	}
+
+	return pal, nil
+}
