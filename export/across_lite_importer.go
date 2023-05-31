75f0511f6559890ddda079194e79415021b2a1df
Phil Hanna <ph1204@gmail.com>
2023-05-31 15:26:33 -0400

    Finished import function

--- export/across_lite_importer.go
+++ export/across_lite_importer.go
@@ -3,6 +3,7 @@ package export
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"regexp"
 	"strconv"
@@ -15,16 +16,19 @@ import (
 
 // Import parses the text in an AcrossLite puzzle
 func Import(filename string) (*AcrossLite, error) {
+
+	// Initialize a new AcrossLite structure
 	pal := new(AcrossLite)
+	pal.grid = make([]string, 0)
+	pal.acrossClues = make([]string, 0)
+	pal.downClues = make([]string, 0)
+	pal.notepad = make([]string, 0)
 
-	fp, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-
+	// Describe the parsing states of the finite state machine
 	type ParsingState byte
 	const (
-		INIT ParsingState = iota
+		UNKNOWN ParsingState = iota
+		INIT
 		LOOKING_FOR_TITLE
 		READING_TITLE
 		LOOKING_FOR_AUTHOR
@@ -40,98 +44,179 @@ func Import(filename string) (*AcrossLite, error) {
 		READING_NOTEPAD
 		DONE
 	)
-	state := INIT
-	scanner := bufio.NewScanner(fp)
-	for scanner.Scan() {
 
-		line := scanner.Text()
-		line = strings.TrimSpace(line)
+	// Define the map of parsing states to handler functions
+	type Handler func(string) (ParsingState, error)
 
-		switch state {
+	stateMap := map[ParsingState]Handler{
 
-		case INIT:
-			if line == "<ACROSS PUZZLE>" {
-				state = LOOKING_FOR_TITLE
-			} else {
-				return nil, fmt.Errorf("Not a valid AcrossLite text file: %s", line)
+		INIT: func(line string) (ParsingState, error) {
+			switch line {
+			case "<ACROSS PUZZLE>":
+				return LOOKING_FOR_TITLE, nil
+			default:
+				return UNKNOWN, fmt.Errorf("Valid AcrossLite file must start with <ACROSS PUZZLE>, not %s", line)
 			}
+		},
 
-		case LOOKING_FOR_TITLE:
-			if line == "<TITLE>" {
-				state = READING_TITLE
-			} else {
-				return nil, fmt.Errorf("did not find <TITLE>")
+		LOOKING_FOR_TITLE: func(line string) (ParsingState, error) {
+			switch line {
+			case "<TITLE>":
+				return READING_TITLE, nil
+			default:
+				return UNKNOWN, fmt.Errorf("did not find <TITLE>")
 			}
+		},
 
-		case READING_TITLE:
+		READING_TITLE: func(line string) (ParsingState, error) {
 			pal.title = line
-			state = LOOKING_FOR_AUTHOR
+			return LOOKING_FOR_AUTHOR, nil
+		},
 
-		case LOOKING_FOR_AUTHOR:
-			if line == "<AUTHOR>" {
-				state = READING_AUTHOR
-			} else {
-				return nil, fmt.Errorf("did not find <AUTHOR>")
+		LOOKING_FOR_AUTHOR: func(line string) (ParsingState, error) {
+			switch line {
+			case "<AUTHOR>":
+				return READING_AUTHOR, nil
+			default:
+				return UNKNOWN, fmt.Errorf("did not find <AUTHOR>")
 			}
+		},
 
-		case READING_AUTHOR:
+		READING_AUTHOR: func(line string) (ParsingState, error) {
 			pal.author = line
-			state = LOOKING_FOR_COPYRIGHT
+			return LOOKING_FOR_COPYRIGHT, nil
+		},
 
-		case LOOKING_FOR_COPYRIGHT:
-			if line == "<COPYRIGHT>" {
-				state = READING_COPYRIGHT
-			} else {
-				return nil, fmt.Errorf("did not find <COPYRIGHT>")
+		LOOKING_FOR_COPYRIGHT: func(line string) (ParsingState, error) {
+			switch line {
+			case "<COPYRIGHT>":
+				return READING_COPYRIGHT, nil
+			default:
+				return UNKNOWN, fmt.Errorf("did not find <COPYRIGHT>")
 			}
+		},
 
-		case READING_COPYRIGHT:
+		READING_COPYRIGHT: func(line string) (ParsingState, error) {
 			pal.copyright = line
-			state = LOOKING_FOR_SIZE
+			return LOOKING_FOR_SIZE, nil
+		},
 
-		case LOOKING_FOR_SIZE:
-			if line == "<SIZE>" {
-				state = READING_SIZE
-			} else {
-				return nil, fmt.Errorf("did not find <SIZE>")
+		LOOKING_FOR_SIZE: func(line string) (ParsingState, error) {
+			switch line {
+			case "<SIZE>":
+				return READING_SIZE, nil
+			default:
+				return UNKNOWN, fmt.Errorf("did not find <SIZE>")
 			}
+		},
 
-		case READING_SIZE:
+		READING_SIZE: func(line string) (ParsingState, error) {
 			reSize := regexp.MustCompile(`(\d+)x(\d)`)
 			tokens := reSize.FindStringSubmatch(line)
-			if tokens == nil {
-				return nil, fmt.Errorf("no <digits>x<digits> size expression found")
+			switch {
+			case tokens == nil:
+				return UNKNOWN, fmt.Errorf("no <digits>x<digits> size expression found")
+			case len(tokens) != 2:
+				return UNKNOWN, fmt.Errorf("expected two size integers, found %d", len(tokens))
+			case tokens[0] != tokens[1]:
+				return UNKNOWN, fmt.Errorf("only square grids allowed, not %sx%s", tokens[0], tokens[1])
+			default:
+				pal.size, _ = strconv.Atoi(tokens[0])
+				return LOOKING_FOR_GRID, nil
 			}
-			if len(tokens) != 2 {
-				return nil, fmt.Errorf("expected two size integers, found %d", len(tokens))
-			}
-			if tokens[0] != tokens[1] {
-				return nil, fmt.Errorf("only square grids allowed, not %sx%s", tokens[0], tokens[1])
-			}
-			pal.size, _ = strconv.Atoi(tokens[0])
-			state = LOOKING_FOR_GRID
+		},
 
-		case LOOKING_FOR_GRID:
-			if line == "<GRID>" {
-				state = READING_GRID
+		LOOKING_FOR_GRID: func(line string) (ParsingState, error) {
+			switch line {
+			case "<GRID>":
 				pal.grid = make([]string, 0)
-			} else {
-				return nil, fmt.Errorf("did not find <GRID>")
+				return READING_GRID, nil
+			default:
+				return UNKNOWN, fmt.Errorf("did not find <GRID>")
 			}
+		},
 
-		case READING_GRID:
+		READING_GRID: func(line string) (ParsingState, error) {
 			if line == "<ACROSS>" {
 				if len(pal.grid) != pal.size {
-					return nil, fmt.Errorf("Must be %d lines in grid, not %d", pal.size, len(pal.grid))
+					return UNKNOWN, fmt.Errorf(
+						"found %d lines in <GRID> section, expected %d",
+						len(pal.grid), pal.size)
 				}
-				state = READING_ACROSS
-				pal.acrossClues = make([]string, 0)
-			} else {
-				return nil, fmt.Errorf("did not find <ACROSS>")
+				return READING_ACROSS, nil
 			}
+			if len(line) != pal.size {
+				return UNKNOWN, fmt.Errorf(
+					"found %d characters in grid line, expected %d",
+					len(line), pal.size)
+			}
+			pal.grid = append(pal.grid, line)
+			return READING_GRID, nil
+		},
 
-		case READING_ACROSS:
+		READING_ACROSS: func(line string) (ParsingState, error) {
+			if line == "<DOWN>" {
+				return READING_DOWN, nil
+			}
+			pal.acrossClues = append(pal.acrossClues, line)
+			return READING_ACROSS, nil
+		},
+
+		READING_DOWN: func(line string) (ParsingState, error) {
+			if line == "<NOTEPAD>" {
+				return READING_NOTEPAD, nil
+			}
+			pal.downClues = append(pal.downClues, line)
+			return READING_DOWN, nil
+		},
+
+		READING_NOTEPAD: func(line string) (ParsingState, error) {
+			pal.notepad = append(pal.notepad, line)
+			return READING_NOTEPAD, nil
+		},
+
+		DONE: func(line string) (ParsingState, error) {
+			return DONE, nil
+		},
+	}
+
+	// Open the file
+	fp, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer fp.Close()
+
+	// -----------------------------------------------------------------
+	// Now run the finite state machine:
+	//
+	// - Set initial state to INIT
+	// - For each line in the file:
+	//   - Lookup the handler function
+	//   - Call the function with this line
+	//   - Set the new state
+	// - Check for a valid ending state
+	// -----------------------------------------------------------------
+
+	state := INIT
+
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.TrimSpace(line)
+		handler, found := stateMap[state]
+		if found {
+			state, err = handler(line)
+			if err != nil {
+				log.Println(err)
+				return nil, err
+			}
 		}
+	}
+
+	if state != DONE {
+		// TODO change this to a switch statement for all states
+		return nil, fmt.Errorf("Expected final state to be DONE, not %v", state)
 	}
 
 	return pal, nil
