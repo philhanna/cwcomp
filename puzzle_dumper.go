9767060c4f936835b3c952155e02a1b17d3f3866
Phil Hanna <ph1204@gmail.com>
2023-06-01 18:40:08 -0400

    Extracted TracePuzzle from DumpPuzzle

--- puzzle_dumper.go
+++ puzzle_dumper.go
@@ -5,23 +5,9 @@ import (
 	"log"
 )
 
-// DumpPuzzle is a diagnostic function that shows the exact composition of
-// each cell in the grid.
+// DumpPuzzle is a diagnostic function that shows the exact composition
+// of each cell in the grid.
 func DumpPuzzle(puzzle *Puzzle) {
-	n := puzzle.n
-	for r := 1; r <= n; r++ {
-		for c := 1; c <= n; c++ {
-			point := NewPoint(r, c)
-			cell := puzzle.GetCell(point)
-			switch typedCell := cell.(type) {
-			case BlackCell:
-				log.Printf("BlackCell    at %v has value %v\n", point, typedCell.String())
-			case LetterCell:
-				log.Printf("LetterCell   at %v has value %v\n", point, typedCell.String())
-			}
-		}
-	}
-	fmt.Println(puzzle.String())
 
 	dumpClues := func(direction Direction) {
 		for _, wn := range puzzle.wordNumbers {
@@ -38,4 +24,24 @@ func DumpPuzzle(puzzle *Puzzle) {
 
 	fmt.Println("Down:")
 	dumpClues(DOWN)
+}
+
+// TracePuzzle is a diagnostic function that shows how each cell is
+// constructed
+func TracePuzzle(puzzle *Puzzle) {
+	n := puzzle.n
+	for r := 1; r <= n; r++ {
+		for c := 1; c <= n; c++ {
+			point := NewPoint(r, c)
+			cell := puzzle.GetCell(point)
+			switch typedCell := cell.(type) {
+			case BlackCell:
+				log.Printf("BlackCell at  %v has value %v\n", point, typedCell.String())
+			case LetterCell:
+				log.Printf("LetterCell at %v has value %v\n", point, typedCell.String())
+			}
+		}
+	}
+	fmt.Println(puzzle.String())
+
 }
