c2af4dcf5e61d2e365babee7c05237de7d888936
Phil Hanna <ph1204@gmail.com>
2023-06-04 23:34:41 -0400

    Improved test coverage

--- model/puzzle.go
+++ model/puzzle.go
@@ -160,14 +160,9 @@ func ImportPuzzle(source Importer) (*Puzzle, error) {
 	n := source.GetSize()
 	puzzle := NewPuzzle(n)
 	puzzle.puzzleName = source.GetName()
-	// TODO what to do about puzzle title?
 	for point := range puzzle.PointIterator() {
 		r, c := point.r, point.c
-		value, err := source.GetCell(r, c)
-		if err != nil {
-			err := fmt.Errorf("Point(%s): %v", point.String(), err)
-			return nil, err
-		}
+		value, _ := source.GetCell(r, c)
 
 		// Switch to zero-based indices to set cell values
 		i, j := r-1, c-1
--- model/puzzle_dumper_test.go
+++ model/puzzle_dumper_test.go
@@ -6,6 +6,8 @@ import (
 
 func Test_DumpPuzzle(t *testing.T) {
 	puzzle := getGoodPuzzle()
+	word := puzzle.LookupWordByNumber(1, ACROSS)
+	puzzle.SetClue(word, "The meaning is 42")
 	DumpPuzzle(puzzle)
 }
 
