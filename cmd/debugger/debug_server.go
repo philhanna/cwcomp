70423b788a1053e1327955b87eded57355f1d131
Phil Hanna <ph1204@gmail.com>
2023-06-23 21:54:26 -0400

    Get URL from request

--- cmd/debugger/debug_server.go
+++ cmd/debugger/debug_server.go
@@ -39,7 +39,8 @@ func (h loggingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+	requestURL := r.URL.String()
+	w.Header().Set("Access-Control-Allow-Origin", requestURL)
 	w.Header().Set("Access-Control-Allow-Headers", "credentials, content-type, access-control-allow-origin")
 	w.WriteHeader(http.StatusOK)
 
